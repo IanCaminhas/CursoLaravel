cd9dcaef2b2de8c433a07cb0dfe81d844ae95d76
IanPC <iancaminhas25@hotmail.com>
2024-02-14 09:51:06 -0300

    Raw expressions

--- 4 - banco_de_dados/routes/web.php
+++ 4 - banco_de_dados/routes/web.php
@@ -6,23 +6,68 @@ use Illuminate\Support\Facades\DB;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-//Objetivo do arquivo: Query Builder#02
+//Objetivo do arquivo: Raw Expressions
 
 Route::get('/', function () {
 
-    /*Sem paginação:
-        $users = DB::table('users')->select('id', 'name', 'email')->get();
+    /* Se eu fizer o código abaixo sem o DB::raw(),
+    aspas vão ser inseridas na query sql e um erro será exibido no navegador.
+    usando DB::raw(), o sql puro será elaborado sem as aspas.
+
+    ->where('id','>',10) id ser maior que 10
+    $users = DB::table('users')
+        ->select(DB::raw('count(*) as user_count, name'))
+        ->where('id','>',10)
+        ->groupBy('name')
+        ->get();
     */
 
-    //Com paginação: ->paginate(10) 10 users por página
-    //dd($users);
-    $usersPaginateNumerate = DB::table('users')->select('id', 'name', 'email')->paginate(10);
+    /* A query builder abaixo também vai resultar erro se
+    eu não colocar o DB::raw().
+     aspas vão ser inseridas na query sql e um erro será exibido no navegador.
+    usando DB::raw(), o sql puro será elaborado sem as aspas.
 
+     $users = DB::table('users')
+        ->select('id', 'name', 'email','created_at')
+        ->where(DB::raw("(DATE_FORMAT(created_at, '%d-%m-%y'))"), "02-12-2024")
+        ->get();
+    */
 
-    //Sem a paginação por números. Vai ter dois links: previous e paginate
-    $usersPatginateNextPrevious = DB::table('users')->select('id', 'name', 'email')->simplePaginate(10);
+    /*"?" vai ser substituída pelo binding: ['02-12-2024']. Assim poderei receber essa data de um formulário
+    $users = DB::table('users')
+        ->select('id', 'name', 'email','created_at')
+        ->where(DB::raw("(DATE_FORMAT(created_at, '%d-%m-%y'))"), "?")
+        ->setBindings(['02-12-2024'])
+        ->get();
+    */
 
-    return view('paginacao')->with('usersPaginateNumerate', $usersPaginateNumerate)
-                            ->with('usersPatginateNextPrevious', $usersPatginateNextPrevious);
+    /*Não preciso trabalhar com DB::raw()... Por consequência, não vou precisar trabalhar com ->setBindings().
+    O valor vai ser passado como segundo parâmetro.
+    O Laravel dispõe de whereRaw, selectRaw, etc... Basta consultar a documentação.
+    https://laravel.com/docs/10.x/queries#raw-expressions
+
+    $users = DB::table('users')
+        ->select('id', 'name', 'email','created_at')
+        ->whereRaw("(DATE_FORMAT(created_at, '%d-%m-%y')) = ?", ['12-02-2024'])
+        ->get();
+
+    */
+
+    /*
+        ? vai ser substituído por ['12-02-2024']
+        Além disso, são necessários o ? e o ['12-02-2024']
+    */
+    $users = DB::table('users')
+    ->select('id', 'name', 'email','created_at')
+    ->whereRaw("(DATE_FORMAT(created_at, '%d-%m-%y')) = ?", ['12-02-2024'])
+    ->get();
+
+    dd($users);
+
+    /*
+        Fazendo dd($users), será retornado: Illuminate\Support\Collection
+    */
+
+    return view('users')->with('users', $users);
 
 });
